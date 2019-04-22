2e17d6290937f1ede0fe82bcd16909974eedd6d8
Nightwingbeyond <30642762+Nightwingbeyond@users.noreply.github.com>
2019-04-22 16:09:09 -0400

    new files

--- client.c
+++ client.c
@@ -1,45 +1,86 @@
-// Client side C/C++ program to demonstrate Socket programming
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#define PORT 8080
+#include "wtf.h"
 
-int main(int argc, char const *argv[])
-{
-    struct sockaddr_in address;
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return -1;
+int firstRun = 1;
+
+void configure(char* address, char* port){
+    int fd_configure = open("./.configure", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd_configure < 0) {
+        printf("Error: Could not create configure file. Please try again.\n");
+        return EXIT_FAILURE;
+    }
+    write(fd_configure, address, strlen(address));
+    write(fd_configure, "\t", 1);
+    write(fd_configure, port, strlen(port));
+    
+    close(fd_configure);
+    return;
+}
+
+int main(int argc, char** argv){
+    struct sockaddr_in serverinfo;
+    struct hostent *serverip;
+    
+    signal(SIGINT, ctrlc_shutdown);
+    
+    //if this is the first run of the client program, execute configure
+    if(firstRun){
+        //check for sufficient arguments
+        if(argc != 3){
+            printf("ERROR: Insufficient arguments to configure.");
+            return EXIT_FAILURE;
+        }
+        //first command must be configure
+        if(!strcmp(argv[1],"configure")){
+            printf("ERROR: First command must be configure");
+            return EXIT_FAILURE;
+        }
+        
+        char* IPaddr = argv[2];
+        char* portNum = argv[3];
+        
+        //call congifure to create a file to store input values
+        configure(IPaddr, portNum);
+        
+        firsRun = 0;
+        return EXIT_SUCCESS;
     }
     
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
+    if(pthread_mutex_init(&lock, NULL)!=0){
+        printf("ERROR: Failed creating mutex \n");
+    }
+
+    if(strcmp(arg[1],"add")){
+        if(argc != 3){
+            printf("ERROR: Insufficient arguments to add file.");
+            return EXIT_FAILURE;
+        }
+        struct dirent *de;
+        DIR *dr;
+        dr = opendir(argv[2]);
+        if(dr == NULL){
+            printf("ERROR: Project does not exist on client.");
+            return EXIT_FAILURE;
+        }
+        //create a file of input name and add it to open directory
+        int newFile = open( argv[3] , O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        //oepn the manifest file
+        int fd_manifest = open(".Manifest", O_WRONLY | O_APPEND);
+        write( fd_manifest, "1",1);
+        write( fd_manifest, "\t",1);
+        write( fd_manifest, argv[3] , strlen(argv[3]));
+        write( fd_manifest, "\t",1);
+        
+        
+    }
+    if(strcmp(arg[1],"remove")){
+        
     }
     
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-    send(sock , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
-    valread = read( sock , buffer, 1024);
-    printf("%s\n",buffer );
+    
+    
+    
+    
+    serverip =
     return 0;
 }
 
---
+++ wtf.h
@@ -0,0 +1,11 @@
+//standard libraries
+#include <stdio.h>
+#include <stdlib.h>
+//for string stuff
+#include <string.h>
+//directory manipulation
+#include <dirent.h>
+#include <netdb.h>
+#include <sys/socket.h>
+//used to initialize and delete mutexes
+#include <pthread.h>
