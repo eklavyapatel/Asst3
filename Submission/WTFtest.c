5362f0646ff468993c7e4ce75b22dd8489772dec
anpatel18 <anp125@scarletmail.rutgers.edu>
2019-05-06 22:41:08 -0400

    Add files via upload

--- Submission/WTFtest.c
+++ Submission/WTFtest.c
@@ -9,31 +9,31 @@ int main(int argc, char**argv){
     if(child != 0){
         wait(NULL);
     } else {
-      exec("./wtfclient configure cp.cs.rutgers.edu 8080");
+      execvp("./wtfclient", "configure", "cp.cs.rutgers.edu", "8080");
       sleep();
-      exec("./wtfclient create Example1");
+      execvp("./wtfclient", "create", "Example1");
       sleep();
-      exec("./wtfclient add File1");
+      execvp("./wtfclient", "add", "File1");
       sleep();
-      exec("./wtfclient add File2");
+      execvp("./wtfclient", "add", "File2");
       sleep();
-      exec("./wtfclient commit Example1");
+      execvp("./wtfclient", "commit", "Example1");
       sleep();
-      exec("./wtfclient push Example1");
+      execvp("./wtfclient", "push", "Example1");
       sleep();
-      exec("./wtfclient remove File2");
+      execvp("./wtfclient", "remove", "File2");
       sleep();
-      exec("./wtfclient update Example1");
+      execvp("./wtfclient", "update", "Example1");
       sleep();
-      exec("./wtfclient upgrade Example1");
+      execvp("./wtfclient", "upgrade", "Example1");
       sleep();
-      exec("./wtfclient history Example1");
+      execvp("./wtfclient", "history", "Example1");
       sleep();
-      exec("./wtfclient currentversion Example1");
+      execvp("./wtfclient", "currentversion", "Example1");
       sleep();
-      exec("./wtfclient rollback Example1 1");
+      execvp("./wtfclient", "rollback", "Example1", "1");
       sleep();
-      exec("./wtfclient destroy Example1");
+      execvp("./wtfclient", "destroy", "Example1");
       sleep();
     }
 
